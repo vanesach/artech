ecd67164edc25aac24b1f1447eb1f81929639427
vanesa chaves <vanesa.artelligence@gmail.com>
2022-07-23 00:08:17 -0300

    se subio app de login

---
+++ aplicaciones/application_5/lib/FireBase_auth.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: file_names
+
+/*import 'package:firebase_auth/firebase_auth.dart';
+// ignore: unused_import
+import 'firebase_option.dart';
+
+Future<UserCredential> createUserWithEmailAndPassword({required String email,required String password,}) async{
+  
+  return UserCredential._delegate( 
+   // this, 
+    await _delegate.createUserWithEmailAndPassword(email, password),
+  );
+}
+
+// ignore: camel_case_types
+class _delegate {
+  static createUserWithEmailAndPassword(String email, String password) {}
+}*/
---
+++ aplicaciones/application_5/lib/views/login_view.dart
@@ -0,0 +1,28 @@
+// ignore: unused_import
+// ignore_for_file: prefer_typing_uninitialized_variables
+
+// ignore: unused_import
+import 'package:firebase_auth/firebase_auth.dart';
+// ignore: unused_import
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+   
+  @override
+  Widget build(BuildContext context) {
+  
+    // ignore: unused_local_variable
+    var defaultFirebaseOptions;
+    return Container();
+}
+}
